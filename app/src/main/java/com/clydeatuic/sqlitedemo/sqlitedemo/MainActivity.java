73eee4cc5c1e0fbaf5c5da06dd65612099bdce0e
MobileDev <mobiledev@Unit19.local>
2017-11-24 10:59:38 +0800

    Initial commit

---
+++ app/src/main/java/com/clydeatuic/sqlitedemo/sqlitedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.clydeatuic.sqlitedemo.sqlitedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
